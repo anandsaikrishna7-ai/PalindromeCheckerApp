7aa53cc3c1ad288c01dd0eac812e8d620dd3a215
anandsaikrishna7-ai <anandsaikrishna7@gmail.com>
2026-02-27 09:43:13 +0530

    UC2Hardcoded

--- src/HardcodedPalindrome.java
+++ src/HardcodedPalindrome.java
@@ -1,5 +1,17 @@
 public class HardcodedPalindrome {
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        System.out.println("Checking word: " + word);
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equals(reversed)) {
+            System.out.println("Result: '" + word + "' is a palindrome.");
+        } else {
+            System.out.println("Result: '" + word + "' is NOT a palindrome.");
+        }
     }
 }
